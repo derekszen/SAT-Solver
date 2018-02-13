7fa602e233c5386a24f3c10237c0de9e9e8268f8
ZHUOXI ZENG <zengzhuoxi@gmail.com>
2018-02-12 22:45:03 -0500

    Skeleton code.

---
+++ main.py
@@ -0,0 +1,14 @@
+# Skeleton Code
+
+
+
+
+def __init__(self):
+    self.variables = []
+    self.variable_table = dict()
+    self.clauses = []
+
+    
+
+if  __name__ == "__main__":
+    print("I love anime.")
